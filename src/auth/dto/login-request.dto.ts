be8a6ed13fedd09daafbdda1e289a174f2c0146f
Ezequias <ezequias.net@gmail.com>
2023-03-01 21:19:05 -0300

    Add DTO validations

--- src/auth/dto/login-request.dto.ts
+++ src/auth/dto/login-request.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class LoginRequestDto {
+  @IsNotEmpty()
   username: string;
+
+  @IsNotEmpty()
   password: string;
 }
--- src/auth/dto/signup-response.dto.ts
+++ src/auth/dto/signup-response.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class SignupResponseDto {
+  @IsNotEmpty()
   token: string;
+
+  @IsNotEmpty()
   userId: string;
 }
